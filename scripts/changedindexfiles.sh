967df531ca981f71c625ef81ed442e83e9ca0c3d
githaefrancis <mureithigithae@gmail.com>
2025-03-18 15:59:29 +0300

    add box transform

--- scripts/changedindexfiles.sh
+++ scripts/changedindexfiles.sh
@@ -8,6 +8,7 @@ if [ "$changed_files" != "" ];then
         DIR=$(dirname "$file")
         echo $DIR
         node scripts/screenshot.js "$file" "$DIR/screengrab.png"
+        git add "$DIR/screengrab.png"
     done
 else
      echo "No new changes to your index files, exiting "
